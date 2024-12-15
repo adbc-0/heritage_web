ce1a534170c7e2feda1b266dae7ae0425d3b9e02
adbc <adrianboczon98@gmail.com>
2024-12-15 07:02:30 +0100

    Fix scroll issue wuth Page Loader

--- react/src/pages/LoadingPage/index.tsx
+++ react/src/pages/LoadingPage/index.tsx
@@ -2,8 +2,12 @@ import { Loader } from "lucide-react";
 
 export function LoadingPage() {
     return (
-        <div className="h-screen flex items-center justify-center">
-            <Loader size={40} className="animate-spin" />
+        <div className="relative h-full w-full">
+            <div className="absolute top-0 left-0 h-full w-full">
+                <div className="h-full flex items-center justify-center">
+                    <Loader size={40} className="animate-spin" />
+                </div>
+            </div>
         </div>
     );
 }
