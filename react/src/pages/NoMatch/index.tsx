50b6daf28cec3696257cd1c4caed35fda7cc6f59
adbc <adrianboczon98@gmail.com>
2024-12-22 14:01:39 +0100

    Add back button to 404 page

--- react/src/pages/NoMatch/index.tsx
+++ react/src/pages/NoMatch/index.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router";
+
+import { Button } from "@/components/ui/button";
+import { RouterPath } from "@/constants/routePaths";
+
 export function NoMatch() {
     return (
         <div className="text-center content-center h-full">
-            <h1 className="text-2xl font-bold tracking-tight mt-2">Page does not exist</h1>
+            <h1 className="text-9xl font-bold tracking-tight mt-2">404</h1>
+            <h1 className="text-4xl font-bold tracking-tight mt-2">Page not found</h1>
+            <Link to={RouterPath.ROOT}>
+                <Button className="mt-6">Go back home</Button>
+            </Link>
         </div>
     );
 }
