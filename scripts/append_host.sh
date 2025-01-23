d15df9ce21d3effd4a605260e64c8d7a4303ebaa
adbc <adrianboczon98@gmail.com>
2025-01-23 23:05:36 +0100

    Tweak script comment

--- scripts/append_host.sh
+++ scripts/append_host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Bind domain to local address for development purposes
+# Bind domain to local address so you will be able to access the project when developing it locally.
 echo "Appending new line to /etc/hosts"
 echo "127.0.0.1	naszrod.local" >> /etc/hosts
