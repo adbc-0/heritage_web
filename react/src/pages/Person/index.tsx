bee1cf03b37f2a0e37531cb1a2a8bc05b43a866c
adbc <adrianboczon98@gmail.com>
2025-02-09 06:13:16 +0100

    Reroute to 404 on person not found

--- react/src/pages/Person/index.tsx
+++ react/src/pages/Person/index.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { Params, useNavigate, useParams } from "react-router";
+
+import { useHeritage } from "@/contexts/heritageContext";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Basic } from "./Basic";
 import { Tree } from "./Tree";
@@ -6,6 +10,32 @@ import { Documents } from "./Documents";
 import { Notes } from "./Notes";
 
 export default function Person() {
+    const navigate = useNavigate();
+    const { id } = useParams<Params>();
+    const { heritage } = useHeritage();
+
+    const [personLoaded, setPersonLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!id) {
+            void navigate("/404", { replace: true });
+            return;
+        }
+        if (!heritage) {
+            void navigate("/404", { replace: true });
+            return;
+        }
+        const person = heritage.indis.find((person) => person.id === id);
+        if (!person) {
+            void navigate("/404", { replace: true });
+        }
+        setPersonLoaded(true);
+    }, [heritage, id, navigate]);
+
+    if (!personLoaded) {
+        return null;
+    }
+
     return (
         <Tabs defaultValue="basic" className="mt-1">
             <div className="overflow-x-auto m-1">
