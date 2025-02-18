7ccbeb7a673f5ffde8d906997862a298fbdb9361
adbc <adrianboczon98@gmail.com>
2025-02-18 22:38:27 +0100

    Improve text formatting

--- react/src/pages/Contact/index.tsx
+++ react/src/pages/Contact/index.tsx
@@ -4,7 +4,7 @@ const CONTACT_MAIL = "m.b.ozimek@gmail.com";
 
 export default function Contact() {
     return (
-        <div className="h-full flex flex-col justify-center gap-4 px-8 py-6 max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl m-auto">
+        <div className="h-full flex flex-col justify-center gap-4 px-8 py-6 max-w-xl md:max-w-3xl m-auto">
             <p>
                 Napisz do mnie jeśli masz jakieś informacje, które chciał/abyś zamieścić na stronie.
             </p>
