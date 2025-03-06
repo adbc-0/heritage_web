c49fcd6a5e65f8f6ef35cae9cf3f65c28d8688ca
adbc <adrianboczon98@gmail.com>
2025-03-06 08:31:26 +0100

    Revert temporary fix

--- react/src/components/views/Layout/MobileHeader.tsx
+++ react/src/components/views/Layout/MobileHeader.tsx
@@ -1,17 +1,8 @@
 import { preload } from "react-dom";
-import { useEffect } from "react";
 
 import LOGO from "@/assets/logo.svg";
 
 export function MobileHeader() {
-    useEffect(() => {
-        document.getElementById("root").dispatchEvent(new MouseEvent("click", { bubbles: true }));
-        setTimeout(() => {
-            document
-                .getElementById("root")
-                .dispatchEvent(new MouseEvent("click", { bubbles: true }));
-        }, 1000);
-    }, []);
     preload(LOGO, { as: "image", type: "image/svg+xml" });
     return (
         <header className="bg-background px-8 py-3 items-center border-b border-border gap-2">
