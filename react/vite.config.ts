88be4aebaf6ce0266e58c1ae90b7df2b623f357f
adbc <adrianboczon98@gmail.com>
2025-03-28 12:37:41 +0100

    Remove test variable from gh action

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-console.log(process.env);
-console.log("VITE_ENV", process.env.VITE_API_URL, process.env.VITE_API_URL?.length);
-
 // https://vite.dev/config/
 export default defineConfig({
     server: {
