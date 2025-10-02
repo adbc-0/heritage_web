3dabf4e218c4dc87882c8e455e9f7fb17d6f3db5
adbc <adrianboczon98@gmail.com>
2025-10-02 23:28:32 +0200

    Show parent when viewing person tree

--- react/src/pages/Person/Tree.tsx
+++ react/src/pages/Person/Tree.tsx
@@ -1,17 +1,63 @@
-import { FamilyGraph } from "@/features/FamilyGraph/FamilyGraph";
 import { type Params, useParams } from "react-router";
 
-// ToDo: what will happen with ozimki root. Will dummy node be rendered?
+import { isNil } from "@/lib/utils";
+import { useHeritage } from "@/contexts/heritageContext";
+import { isPersonInvisible, searchFamily, searchPerson } from "@/features/FamilyGraph/utils";
+import { FamilyGraph } from "@/features/FamilyGraph/FamilyGraph";
+
+import { HeritageRaw } from "@/types/heritage.types";
+
+function addParentWhenExists(heritage: HeritageRaw, parentId: string | null) {
+    if (!parentId) {
+        return [];
+    }
+    const parent = searchPerson(heritage, parentId);
+    if (!parent) {
+        throw new Error("no searched person");
+    }
+    return [parent];
+}
+
+function getParentId(heritage: HeritageRaw, personId: string) {
+    const person = searchPerson(heritage, personId);
+    if (!person) {
+        throw new Error("no searched person");
+    }
+
+    const parentFamily = searchFamily(heritage, person.famc);
+    if (!parentFamily) {
+        return null;
+    }
+
+    // ToDo: Both families should be rendered
+    const [parent] = [
+        ...addParentWhenExists(heritage, parentFamily.husb),
+        ...addParentWhenExists(heritage, parentFamily.wife),
+    ].filter((person) => !isPersonInvisible(person));
+
+    if (!parent) {
+        return null;
+    }
+    return parent.id;
+}
+
 export function Tree() {
     const { id: personId } = useParams<Params>();
+    const { heritage } = useHeritage();
 
-    if (!personId) {
-        throw new Error("cannot draw tree for blank person");
+    if (!heritage) {
+        throw new Error("missing heritage");
     }
+    if (!personId) {
+        throw new Error("missing person parameter");
+    }
+
+    const maybeParentId = getParentId(heritage, personId);
+    const rootPerson = isNil(maybeParentId) ? personId : maybeParentId;
 
     return (
         <div className="bg-background border-t border-border h-full">
-            <FamilyGraph rootPerson={personId} />
+            <FamilyGraph rootPerson={rootPerson} />
         </div>
     );
 }
