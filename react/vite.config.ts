4a2adc4d021f79d1cc43a65781c3988b880d4b58
adbc <adrianboczon98@gmail.com>
2025-10-19 02:52:04 +0200

    Fix: move out babel plugin from dev dependencies

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     plugins: [
         react({
             babel: {
-                plugins: ["babel-plugin-react-compiler"],
+                plugins: [["babel-plugin-react-compiler"]],
             },
         }),
         tailwindcss(),
