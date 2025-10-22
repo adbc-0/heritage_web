e0b216fb6edc75c4e289b63f51d11b6dc6fe99ae
adbc <adrianboczon98@gmail.com>
2025-10-23 00:06:17 +0200

    chore: config files cleanup

---
+++ react/README.md
@@ -0,0 +1,6 @@
+Project was initialized with Vite
+
+Typescript config files: tsconfig.app.json, tsconfig.json, tsconfig.node.json
+
+tsconfig.node.json is used by vite running locally
+tsconfig.app.json is used by the app
--- react/eslint.config.js
+++ react/eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -8,8 +8,8 @@ import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
-export default defineConfig(
-    { ignores: ["dist"] },
+export default defineConfig([
+    globalIgnores(["dist"]),
     {
         settings: { react: { version: "19.2.0" } },
         extends: [
@@ -17,19 +17,25 @@ export default defineConfig(
             ...tseslint.configs.strictTypeChecked,
             ...tseslint.configs.stylisticTypeChecked,
         ],
+        // https://eslint.org/docs/latest/use/configure/configuration-files
+        // By default, ESLint lints files that match the patterns **/*.js, **/*.cjs, and **/*.mjs.
+        // Those files are always matched unless you explicitly exclude them using global ignores.
         files: ["**/*.{ts,tsx}"],
         languageOptions: {
             ecmaVersion: "latest",
             globals: globals.browser,
+            // https://typescript-eslint.io/getting-started/typed-linting/
+            // Some typescript-eslint rules utilize TypeScript's type checking APIs to provide much deeper insights into your code.
+            // This requires TypeScript to analyze your entire project instead of just the file being linted.
+            // As a result, these rules are slower than traditional lint rules but are much more powerful.
             parserOptions: {
-                project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-                tsconfigRootDir: import.meta.dirname,
+                projectService: true,
             },
         },
         plugins: {
             react,
             "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
+            "react-refresh": reactRefresh, // Validate that your components can safely be updated with Fast Refresh
             "jsx-a11y": jsxA11y,
         },
         rules: {
@@ -38,11 +44,10 @@ export default defineConfig(
             ...reactHooks.configs.recommended.rules,
             ...jsxA11y.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-            "react-hooks/exhaustive-deps": "off",
             "@typescript-eslint/consistent-type-definitions": "off",
             "@typescript-eslint/no-unnecessary-condition": "off",
             "react/prop-types": "off",
             ...prettier.rules,
         },
     },
-);
+]);
