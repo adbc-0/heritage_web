62235c6793b9be6b7b71f841b92a9ba942347aa6
adbc <adrianboczon98@gmail.com>
2025-10-23 08:19:07 +0200

    chore: add some spacing

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import { App } from "./App.tsx";
+import "./index.css";
 
 if (import.meta.env.DEV) {
     console.warn("app running in dev mode");
 }
+
 const rootElement = document.getElementById("root");
 if (!rootElement) {
     throw new Error("no root element");
 }
+
 createRoot(rootElement).render(
     <StrictMode>
         <App />
