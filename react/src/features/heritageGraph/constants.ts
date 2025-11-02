dc6ca2fe60cc4bc0442af8aa34153b6fb7a2ff5c
adbc <adrianboczon98@gmail.com>
2025-11-02 20:47:28 +0100

    feat: Make node wider

--- react/src/features/heritageGraph/constants.ts
+++ react/src/features/heritageGraph/constants.ts
@@ -1,4 +1,4 @@
-export const NODE_WIDTH = 280;
+export const NODE_WIDTH = 350;
 export const NODE_HEIGHT = 80;
 export const VERTICAL_SPACE_BETWEEN_NODES = 70;
 export const HORIZONTAL_SPACE_BETWEEN_NODES = 50;
