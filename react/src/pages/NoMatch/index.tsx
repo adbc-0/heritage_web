fa8365118ced52159ed085372da0be62b1f2add5
adbc <adrianboczon98@gmail.com>
2026-01-21 22:35:55 +0100

    remove unused radix components

--- react/src/pages/NoMatch/index.tsx
+++ react/src/pages/NoMatch/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router";
 
-import { Button } from "@/components/ui/button";
 import { RouterPath } from "@/constants/routePaths";
 
 export function NoMatch() {
@@ -9,7 +8,7 @@ export function NoMatch() {
             <h1 className="text-9xl font-bold tracking-tight mt-2">404</h1>
             <h1 className="text-4xl font-bold tracking-tight mt-2">Page not found</h1>
             <Link to={RouterPath.ROOT}>
-                <Button className="mt-6 cursor-pointer active:scale-95 transition-transform">Go back home</Button>
+                <button type="button" className="mt-6 cursor-pointer active:scale-95 transition-transform">Go back home</button>
             </Link>
         </div>
     );
