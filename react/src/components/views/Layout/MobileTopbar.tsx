f36e4db9371a1142e9837214f81f421dbc14edcb
adbc <58139934+adbc-0@users.noreply.github.com>
2026-02-03 20:16:46 +0100

    feat: add fullscreen mobile search (#37)

--- react/src/components/views/Layout/MobileTopbar.tsx
+++ react/src/components/views/Layout/MobileTopbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { preload } from "react-dom";
 import { useLocation, useNavigate } from "react-router";
 import { clsx } from "clsx";
@@ -6,18 +6,23 @@ import { clsx } from "clsx";
 import { RouterPath } from "@/constants/routePaths.ts";
 import { useGlobalSearch } from "@/features/globalSearch/globalSearch.ts";
 
+import { MdIconButton } from "@/components/ui/MdIconButton/MdIconButton";
+
 import LOGO from "@/assets/logo.svg";
+
 import styles from "./styles.module.css";
 
 export function MobileTopbar() {
     preload(LOGO, { as: "image", type: "image/svg+xml" });
 
-    const dialogRef = useRef<HTMLDialogElement>(null);
+    const mobileSearchInputRef = useRef<HTMLInputElement>(null);
+    const mobileSearchDialogRef = useRef<HTMLDialogElement>(null);
+    const navigationRailDialogRef = useRef<HTMLDialogElement>(null);
 
     // close dialog on clicking backdrop
     // maybe in the future closedby="any" won't be leaking events through backdrop
     useEffect(() => {
-        const dialog = dialogRef.current;
+        const dialog = navigationRailDialogRef.current;
 
         if (!dialog) {
             throw new Error("missing dialog element reference");
@@ -25,7 +30,7 @@ export function MobileTopbar() {
 
         const onDialogClick = (event: MouseEvent) => {
             if (event.target === dialog) {
-                dialog.close();
+                dialog.close("light-dismiss");
             }
         };
 
@@ -40,87 +45,12 @@ export function MobileTopbar() {
     const navigate = useNavigate();
     const { query, searchResults, search, clear } = useGlobalSearch();
 
-    const [searchOpen, setSearchOpen] = useState(false);
-
-    const dockedContainer = searchOpen && searchResults.length > 0;
     const moreResultsText = searchResults.length > 5;
 
     return (
         <nav className={styles.top_bar}>
             {location.pathname !== RouterPath.OSOBY ? (
                 <div className={styles.input_wrapper}>
-                    <input
-                        autoComplete="off"
-                        name="global_search"
-                        className={clsx(styles.search, {
-                            [styles.open_search as unknown as string]: dockedContainer,
-                        })}
-                        type="text"
-                        placeholder="Szukaj"
-                        value={query}
-                        onChange={(e) => {
-                            search(e.target.value);
-                        }}
-                        onFocus={() => {
-                            setSearchOpen(true);
-                        }}
-                        onBlur={() => {
-                            setSearchOpen(false);
-                        }}
-                    />
-                    <div className={styles.leading_icon}>
-                        <button
-                            type="button"
-                            className={clsx("material-symbols-outlined", styles.leading_icon_style)}
-                            command="show-modal"
-                            commandfor="navigation_rail"
-                        >
-                            menu
-                        </button>
-                    </div>
-                    <div className={styles.trailing_icon}>
-                        <span className={clsx("material-symbols-outlined", styles.trailing_icon_style)}>search</span>
-                    </div>
-                    <div className={styles.docked_container}>
-                        {dockedContainer && (
-                            <>
-                                <div className={styles.search_list}>
-                                    {searchResults.slice(0, 5).map((person) => (
-                                        <button
-                                            key={person.id}
-                                            type="button"
-                                            className={clsx(styles.search_list_item, {
-                                                [styles.has_last_list_element as unknown as string]: !moreResultsText,
-                                            })}
-                                            onMouseDown={() => {
-                                                void navigate(`/osoby/${person.id}`);
-                                                clear();
-                                            }}
-                                        >
-                                            {person.firstName} {person.nickName} {person.lastName}
-                                        </button>
-                                    ))}
-                                </div>
-                                {moreResultsText && (
-                                    <>
-                                        <div className={styles.break_line} />
-                                        <button
-                                            className={clsx(styles.search_list_item, styles.has_last_list_element)}
-                                            onMouseDown={() => {
-                                                void navigate(`${RouterPath.OSOBY}?search=${query}`);
-                                                clear();
-                                            }}
-                                        >
-                                            Zobacz pozostałe wyniki ({searchResults.length - 5})
-                                        </button>
-                                    </>
-                                )}
-                            </>
-                        )}
-                    </div>
-                </div>
-            ) : (
-                <div>
                     <button
                         type="button"
                         className={clsx("material-symbols-outlined", styles.leading_icon_style)}
@@ -129,9 +59,100 @@ export function MobileTopbar() {
                     >
                         menu
                     </button>
+                    <button
+                        type="button"
+                        className={styles.search}
+                        onClick={() => {
+                            if (!mobileSearchDialogRef.current) {
+                                throw new Error("missing element reference: mobileSearchDialogRef");
+                            }
+                            if (!mobileSearchInputRef.current) {
+                                throw new Error("missing element reference: mobileSearchInputRef");
+                            }
+                            mobileSearchDialogRef.current.showModal();
+                            mobileSearchInputRef.current.focus();
+                        }}
+                    >
+                        Szukaj
+                    </button>
+                </div>
+            ) : (
+                <div className={styles.simple_menu_wrapper}>
+                    <button
+                        type="button"
+                        className={clsx("material-symbols-outlined", styles.simple_menu_icon)}
+                        command="show-modal"
+                        commandfor="navigation_rail"
+                    >
+                        menu
+                    </button>
                 </div>
             )}
-            <dialog ref={dialogRef} id="navigation_rail" closedby="closerequest" className={styles.modal}>
+            <dialog ref={mobileSearchDialogRef} className={styles.search_dialog} id="search_dialog" closedby="any">
+                <div>
+                    <div className={styles.search_open_block}>
+                        <MdIconButton
+                            iconName="arrow_back"
+                            className={styles.mobile_search_back_button}
+                            command="close"
+                            commandfor="search_dialog"
+                        />
+                        <input
+                            ref={mobileSearchInputRef}
+                            autoComplete="off"
+                            name="global_search"
+                            className={clsx(styles.mobile_search)}
+                            type="text"
+                            placeholder="Szukaj"
+                            value={query}
+                            onChange={(e) => {
+                                search(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className={styles.break_line} />
+                    <div className={styles.search_list}>
+                        {searchResults.slice(0, 5).map((person) => (
+                            <button
+                                key={person.id}
+                                type="button"
+                                className={clsx(styles.search_list_item, {
+                                    [styles.has_last_list_element as unknown as string]: !moreResultsText,
+                                })}
+                                onMouseDown={() => {
+                                    if (!mobileSearchDialogRef.current) {
+                                        throw new Error("missing element reference: mobileSearchDialogRef");
+                                    }
+                                    mobileSearchDialogRef.current.close();
+                                    void navigate(`/osoby/${person.id}`);
+                                    clear();
+                                }}
+                            >
+                                {person.firstName} {person.nickName} {person.lastName}
+                            </button>
+                        ))}
+                    </div>
+                    {moreResultsText && (
+                        <>
+                            <div className={styles.break_line} />
+                            <button
+                                className={clsx(styles.search_list_item, styles.has_last_list_element)}
+                                onMouseDown={() => {
+                                    if (!mobileSearchDialogRef.current) {
+                                        throw new Error("missing element reference: mobileSearchDialogRef");
+                                    }
+                                    mobileSearchDialogRef.current.close();
+                                    void navigate(`${RouterPath.OSOBY}?search=${query}`);
+                                    clear();
+                                }}
+                            >
+                                Zobacz pozostałe wyniki ({searchResults.length - 5})
+                            </button>
+                        </>
+                    )}
+                </div>
+            </dialog>
+            <dialog ref={navigationRailDialogRef} id="navigation_rail" closedby="closerequest" className={styles.modal}>
                 <ul className={styles.navigation_rail}>
                     <button
                         type="button"
